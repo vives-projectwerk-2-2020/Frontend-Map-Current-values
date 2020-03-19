4cb00b79dbef365e645cdd4a3f14d271caf11d03
Eric Schuitmaker <ericschuitmaker@gmail.com>
2020-03-19 17:31:54 +0100

    tried to get svg to update

--- Project Map and SVG/svg.js
+++ Project Map and SVG/svg.js
@@ -1,6 +1,31 @@
-function loadSVG(){
-    console.log("ksfjbvakgfugka")
+function loadSVG() {
     var SVGDiv = document.getElementById("SVGDiv");
-    SVGDiv.innerHTML = "<H1>SVG:</H1></br>" + '<object data="./svg.svg" type="image/svg+xml" id="SVG" width="80%" height="80%"></object>';
+    SVGDiv.innerHTML = '<object data="./svg.svg" type="image/svg+xml" id="SVG" width="80%" height="80%"></object>';
+    setTimeout(function(){updateSVG(); }, 1000);
+    
+    console.log("ksfjbvakgfugka")
 
+    //updateSVG();
+}
+function calcHeigth(value, maxValue, minValue, rectHeight) {
+    var diff = maxValue - minValue;
+    var newHeight = (value - minValue) / (diff / rectHeight)
+    return rectHeight - newHeight;
+}
+function updateSVG() {
+    console.log("Done")
+    //document.getElementById("SVGDiv").addEventListener("load", function () {
+        var SVGDiv = document.getElementById("SVG");
+        var svgDoc = SVGDiv.contentDocument;
+        console.log(svgDoc)
+        var leftBar = svgDoc.getElementById("leftGreenBackground");
+        var rightBar = svgDoc.getElementById("rightGreenBackground");
+        var midBar = svgDoc.getElementById("midGreenBackground");
+        for (var i = 0; i < 50; i++) {
+            var height = calcHeigth(i, 0, 50, 200)
+            leftBar.setAttribute("heigt", height)
+            rightBar.setAttribute("heigt", height)
+            midBar.setAttribute("heigt", height)
+        }
+    //})
 }
