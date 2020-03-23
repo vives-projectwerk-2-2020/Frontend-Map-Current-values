e8b8bcb01dd9ff6a83415e7fc3885058b6aa7743
Eric Schuitmaker <ericschuitmaker@gmail.com>
2020-03-23 15:49:14 +0100

    SVG manipulation done needs testing on server

--- Project Map and SVG/svg.js
+++ Project Map and SVG/svg.js
@@ -1,53 +1,34 @@
 function loadSVG() {
-    var SVGDiv = document.getElementById("SVGDiv");
-    SVGDiv.innerHTML = '<object data="./svg.svg" type="image/svg+xml" id="SVG" width="80%" height="80%"></object>';
-    //setTimeout(function(){updateSVG(); }, 1000);
-    let stateCheck = setInterval(() => {
-        if (document.readyState === 'complete') {
-            clearInterval(stateCheck);
-            // document ready
-            //updateSVG()
-            getImage();
-            console.log("ksfjbvakgfugka")
+    var SVGimage = document.getElementById("SVGimage")
+    var svgDoc
+    //})
+    SVGimage.addEventListener("load", function () {
+        console.log("loaded")
+        svgDoc = SVGimage.getSVGDocument();
+        console.log(SVGimage)
+        console.log(svgDoc)
+        // console.log(SVGimage.contentDocument)
+        var leftBar = svgDoc.getElementById("leftGreenBackground");
+        console.log(leftBar)
+        leftBar.setAttribute("height", 100)
+    })
 
-        }
-    }, 100);
 
-    //updateSVG();
+    // var leftBar = svgDoc.getElementById("leftGreenBackground");
+    // console.log(leftBar)
+    // var rightBar = svgDoc.getElementById("rightGreenBackground");
+    // var midBar = svgDoc.getElementById("midGreenBackground");
+    // for (var i = 0; i < 50; i++) {
+    //     var height = calcHeigth(i, 0, 50, 200)
+    //     leftBar.setAttribute("height", height)
+    //     rightBar.setAttribute("height", height)
+    //     midBar.setAttribute("height", height)
+    //     console.log(height)
+    // }
+
 }
 function calcHeigth(value, maxValue, minValue, rectHeight) {
     var diff = maxValue - minValue;
     var newHeight = (value - minValue) / (diff / rectHeight)
     return rectHeight - newHeight;
-}
-function getImage() {
-    var SVGimage = document.getElementById("SVG")
-    console.log(SVGimage)
-    // SVGimage.addEventListener("load", function () {
-    console.log("Done")
-
-    var SVGDiv = document.getElementById("SVGDiv");
-    var svgDoc = SVGDiv.contentDocument;
-    setTimeout(function(){updateSVG(); }, 1000);
-}
-function updateSVG(content_Document) {
-    // var SVGimage = document.getElementById("SVG")
-    // console.log(SVGimage)
-    // // SVGimage.addEventListener("load", function () {
-    // console.log("Done")
-
-    // var SVGDiv = document.getElementById("SVGDiv");
-    // var svgDoc = SVGDiv.contentDocument;
-    console.log(content_Document)
-    svgDoc = content_Document;
-    var leftBar = svgDoc.getElementById("leftGreenBackground");
-    var rightBar = svgDoc.getElementById("rightGreenBackground");
-    var midBar = svgDoc.getElementById("midGreenBackground");
-    for (var i = 0; i < 50; i++) {
-        var height = calcHeigth(i, 0, 50, 200)
-        leftBar.setAttribute("heigt", height)
-        rightBar.setAttribute("heigt", height)
-        midBar.setAttribute("heigt", height)
-    }
-    // })
 }
